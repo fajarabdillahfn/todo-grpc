08bb8cff22c88c85e3339db8d235f7b73e97f14a
fajar-abdillahfn <abdillah.fajar@tokopedia.com>
2023-06-01 04:08:25 +0700

    feat: remove deleted at and do hard delete

--- internal/model/task.go
+++ internal/model/task.go
@@ -2,16 +2,13 @@ package model
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Task struct {
-	ID          uint           `gorm:"primaryKey" json:"id"`
-	Title       string         `gorm:"uniqueIndex" json:"title"`
-	Description string         `json:"description"`
-	IsCompleted bool           `json:"is_complete"`
-	CreatedAt   time.Time      `json:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+	ID          uint      `gorm:"primaryKey" json:"id"`
+	Title       string    `gorm:"uniqueIndex" json:"title"`
+	Description string    `json:"description"`
+	IsCompleted bool      `json:"is_complete"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
