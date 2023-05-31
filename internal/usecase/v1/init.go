374d7e3916ec0517ee87df2f1e85d8c7e351119a
fajar-abdillahfn <abdillah.fajar@tokopedia.com>
2023-06-01 06:03:05 +0700

    fix: usecase implementation interface

--- internal/usecase/v1/init.go
+++ internal/usecase/v1/init.go
@@ -2,13 +2,14 @@ package v1
 
 import (
 	"github.com/fajarabdillahfn/todo-grpc/internal/repository"
+	"github.com/fajarabdillahfn/todo-grpc/internal/usecase"
 )
 
 type taskUseCaseV1 struct {
 	taskRepo repository.TaskRepository
 }
 
-func NewTaskUseCaseV1(rTask repository.TaskRepository) repository.TaskRepository {
+func NewTaskUseCaseV1(rTask repository.TaskRepository) usecase.TaskUseCase {
 	return &taskUseCaseV1{
 		taskRepo: rTask,
 	}
