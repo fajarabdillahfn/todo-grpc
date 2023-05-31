ac790d99f8d2d91a1c0278f8f077fec39dce9272
fajar-abdillahfn <abdillah.fajar@tokopedia.com>
2023-06-01 04:43:22 +0700

    feat: add main app function

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	grpcTaskDelivery "github.com/fajarabdillahfn/todo-grpc/internal/delivery/grpc"
+	pgTaskRepo "github.com/fajarabdillahfn/todo-grpc/internal/repository/postgres"
+	v1TaskUseCase "github.com/fajarabdillahfn/todo-grpc/internal/usecase/v1"
+	"github.com/fajarabdillahfn/todo-grpc/pkg/db/postgres"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	db := postgres.OpenDB()
+
+	taskRepo := pgTaskRepo.NewPostgresTaskRepository(db)
+	taskUseCase := v1TaskUseCase.NewTaskUseCaseV1(taskRepo)
+
+	netListen, err := net.Listen("tcp", os.Getenv("PORT"))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err.Error())
+	}
+
+	server := grpc.NewServer()
+	grpcTaskDelivery.NewTaskServerGrpc(server, taskUseCase)
+
+	log.Printf("Server start at %v", netListen.Addr())
+	err = server.Serve(netListen)
+	if err != nil {
+		log.Fatalf("Failed to serve: %v", err.Error())
+	}
 }
